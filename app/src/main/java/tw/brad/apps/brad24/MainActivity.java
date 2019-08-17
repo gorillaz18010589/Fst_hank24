177714b65436323b9c81215da75ba5630a34f507
joe <skyjoe0722@gmail.com>
2019-08-17 11:23:24 +0800

    08-17

---
+++ app/src/main/java/tw/brad/apps/brad24/MainActivity.java
@@ -0,0 +1,54 @@
+package tw.brad.apps.brad24;
+//目的:撥放音樂
+//activity以後就寫uxui的東西,網路東西就給Service
+//只要這件事情是可以在背景中處理就用Service
+//前景是觸發Service
+//你可以透過推撥去叫出程式
+//1.
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private  MyReceiver myReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv= findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(myReceiver);//當結束時取消廣播
+        super.onDestroy();
+    }
+
+    //啟動連接到severice頁面
+    public void test1(View view){
+        Intent intent = new Intent(this,MyService.class);//從這個頁面到Server頁面
+        startService(intent);//開始連線到那個Servire
+
+    }
+        //改變i值
+    public void test2(View view){
+        Intent intent = new Intent(this,MyService.class);
+        intent.putExtra("data",(int)( Math.random()*1000));//這邊給值給servire
+        startService(intent);//開始連線到那個Servire
+
+    }
+        //關閉
+    public void test3(View view){
+        Intent intent = new Intent(this,MyService.class);
+        stopService(intent);//關閉連線到Servire
+    }
+
+    //接收廣播的i值,並且顯示在頁面上
+}
---
+++ app/src/main/java/tw/brad/apps/brad24/MyService.java
@@ -0,0 +1,73 @@
+package tw.brad.apps.brad24;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+    public MyService() {//建構是很少用
+    }
+    private Timer timer;
+    private  int i = 0;
+    @Override
+    public IBinder onBind(Intent intent) { //這個方法是抽象方法需要實做,用再綁定型上面
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented"); 一開始就拋出例外,但我沒有要處理榜定型,所已注解
+        return null;
+    }
+
+    //3個狀態看一下
+    @Override
+    public void onCreate() { //只有第一次創造時會叫到
+        super.onCreate();
+        Log.v("brad","onCreate");
+        timer = new Timer();
+        timer.schedule(MyTask,);
+    }
+
+    //創建一個一秒一值家的任務
+    private  class  MyTask extends TimerTask{
+        @Override
+        public void run() {
+            if(i %10 == 0){ //如果被10整除,把事情芳生出去,用廣播方式
+                Intent intent = new Intent("brad");
+                intent.putExtra("i",i); //把這個i掛上去
+                sendBroadcast(intent); //廣播發送出去
+
+            }
+
+            Log.v("brad","i =" + i++);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {//主要都在玩這個帶參數
+//        String name = intent.getStringExtra("data");
+//        Log.v("brad","onStartCommand");
+
+        //抓到Activity的i值
+        int newi = intent.getIntExtra("data",-1);
+        if(newi != -1){ //如果有收到值,把activity的i改變這邊的值
+            i = newi;
+        }
+
+
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("brad"," onDestroy");
+
+        if(timer != null){
+            timer.cancel();
+            timer.purge();
+        }
+    }
+}
